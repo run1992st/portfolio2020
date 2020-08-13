029ba7d320baa38a7cc0dbbfb2d9a926aba2f8f3
Ron Rungsardthong <run1992st@gmail.com>
2020-08-13 09:06:05 +0700

    Cleaning unused asset files.

--- src/main.js
+++ src/main.js
@@ -1,10 +1,9 @@
 import Vue from "vue";
 import App from "./App.vue";
 import "@/assets/tailwind.css";
-import drag from "v-drag";
+
 
 Vue.config.productionTip = false;
-Vue.use(drag);
 
 new Vue({
   render: (h) => h(App),
